ac1c09f8b619b8f27f30f6dc57ad7ced64d4687b
arpruss <arpruss@gmail.com>
2018-05-20 16:35:41 -0500

    foo

--- examples/vectordisplaytest/vectordisplaytest.ino
+++ examples/vectordisplaytest/vectordisplaytest.ino
@@ -1,30 +1,28 @@
 #include <VectorDisplay.h>
 
-VectorDisplay<decltype(Serial)> SerialDisplay(Serial);
-
 void setup() {
   Serial.begin(115200);
   
   pinMode(LED_BUILTIN, OUTPUT);
 
   digitalWrite(LED_BUILTIN, 0);
-  delay(8000);
+  delay(2000);
   digitalWrite(LED_BUILTIN, 1);
   SerialDisplay.clear();
-  SerialDisplay.message("Hello");
 }
 
 uint16_t y=0;
 
 void loop() {
 
+//  Serial.println("VECTORDISPLAYTEST");
 //  delay(100);
 //  digitalWrite(LED_BUILTIN, 1);
 //  delay(100);
   y = (y + 1)%240;
-    SerialDisplay.foreColor(0xFF000000 | ((uint32_t)y<<16));
+  SerialDisplay.foreColor(0xFF000000 | ((uint32_t)y<<16));
   SerialDisplay.line(0, y, 319, 239);
   SerialDisplay.text(50, 20, String(y));  
-  delay(5);
+  delay(15);
 }
 
